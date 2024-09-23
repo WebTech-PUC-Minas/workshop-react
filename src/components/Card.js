b7a388d532106800fda5aa5cbfb5e90279016584
Mariana <marianaalmeidafga@gmail.com>
2024-09-23 16:32:29 -0300

    fix: corrigindo style

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import style from "./Card.module.css";
 
-function Card() {
+function Card({ banner, title, date }) {
+  const [show, setShow] = useState(false);
+  const handleClick = () => {
+    setShow(!show);
+  };
+
   return (
     <div className={style.card}>
       <figure>
-        <img src="/img/movie_01.jpeg" />
+        <img src={banner} />
       </figure>
       <article>
-        <h1>Loren ipsum dolor</h1>
+        <h1>{title}</h1>
         <div>
-          <time>05 de set de 2024</time>
-          <img className={style.icon} src="icons/heart-regular.svg" />
+          <time>{date}</time>
+          {show ? (
+            <img className={style.icon} src="icons/heart-solid.svg" onClick={handleClick}/>
+          ) : (
+            <img className={style.icon} src="icons/heart-regular.svg" onClick={handleClick}/>
+          )}
         </div>
       </article>
     </div>
--- src/pages/React.js
+++ src/pages/React.js
@@ -3,10 +3,10 @@ import Card from "../components/Card";
 function React() {
   return (
     <container className="container">
-      <Card />
-      <Card />
-      <Card />
-    <Card />
+      <Card banner="img/movie_01.jpeg" title="Eduardo e Mônica" date="23 de set de 2024" />
+      <Card banner="img/movie_02.jpg" title="Guardiões da Galáxia" date="24 de set de 2024" />
+      <Card banner="img/movie_03.jpeg" title="Deadpool e Wolverine" date="25 de set de 2024" />
+      <Card banner="img/movie_04.jpg" title="Oppenheimer" date="26 de set de 2024" />
     </container>
   );
 }
